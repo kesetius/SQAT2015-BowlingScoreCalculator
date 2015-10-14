03e4d786e3713a5f0e17862d63ba8bb950a19c06
somename <someemail>
2015-10-14 15:32:44 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -93,6 +93,9 @@ public class TestBowlingScoreCalculator {
 		this.bg.addFrame( new Frame(4,5)  );
 		this.bg.addFrame( new Frame(8,1)  );
 		this.bg.addFrame( new Frame(2,6)  );
+		
+		assertEquals( 94, this.bg.score());
+		
 	}
 	
 	
